4d1a520e69d698384fac2157fa0fa03fec93e30c
Harry Barr <harrybarr530@gmail.com>
2024-09-06 15:13:11 +0100

    added an audio btn

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ import FetchWrapper from "./fetch-wrapper.js";
 const searchBtn = document.querySelector(".search-btn");
 const wordContainer = document.querySelector(".word-container");
 const searchInput = document.querySelector(".search");
+const form = document.querySelector(".input");
+const list = document.querySelector(".list");
 const API = new FetchWrapper(
   "https://api.dictionaryapi.dev/api/v2/entries/en/"
 );
@@ -10,8 +12,10 @@ const cleanWord = function () {
   searchInput.value.trim().replaceAll(" ", "-").toLowerCase();
 };
 
-const searchForWord = async function () {
+const searchForWord = async function (e) {
+  e.preventDefault();
   wordContainer.innerHTML = "";
+  list.innerHTML = "";
   if (!searchInput.value) {
     return alert("Please enter a word");
   } else {
@@ -20,21 +24,33 @@ const searchForWord = async function () {
   try {
     const data = await API.get(`${searchInput.value}`);
     data.forEach((d) => {
-      console.log(d.word);
+      console.log(d);
+    });
+    const playAudio = function () {
+      data.forEach((d) => {
+        const audioSrc = d.phonetics[0].audio;
+        const audioEl = document.createElement("audio");
+        audioEl.setAttribute("src", `${audioSrc}`);
+        audioEl.setAttribute("autoplay", "autoplay");
+      });
+    };
+    data.forEach((d) => {
       wordContainer.insertAdjacentHTML(
         "beforeend",
-        `<h1 class="word">${d.word}    :     ${d.phonetic}</h1>`
+        `<h1 class="word">${d.word} ${
+          d?.phonetic ?? ""
+        }</h1><button class="listen-btn""><i class="fa-solid fa-volume-high"></i></button>`
       );
+      document
+        .querySelector(".listen-btn")
+        .addEventListener("click", playAudio);
     });
     data.forEach((d) => {
-      console.log(d.meanings);
       d.meanings.forEach((l) => {
-        console.log(l.definitions);
         l.definitions.forEach((b) => {
-          console.log(b.definition);
-          wordContainer.insertAdjacentHTML(
+          list.insertAdjacentHTML(
             "beforeend",
-            `<p class="defintions">${b.definition}</p>`
+            `<li class="defintions">${b.definition}</li>`
           );
         });
       });
@@ -43,4 +59,5 @@ const searchForWord = async function () {
     console.error(error);
   }
 };
+form.addEventListener("submit", searchForWord);
 searchBtn.addEventListener("click", searchForWord);
